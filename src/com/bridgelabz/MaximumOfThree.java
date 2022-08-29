d6c4ba6ccc472dbad85ef4241e6875dbabe11d85
amolmane7471 <maneamol3233@gmail.com>
2022-08-29 10:19:28 +0530

    [Amol] : added Maximum problem using generics

---
+++ src/com/bridgelabz/MaximumOfThree.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+/*
+ * purpose : Find Maximum Of Three Numbers Using Generics
+ * author : Amol
+ * since : 2022/08/27
+ */
+public class MaximumOfThree {
+		
+		public static void main(String[] args) {
+
+			System.out.println("*** Welcome To Computing Maximum Of Three Numbers Using Java Generics ***");
+
+		}
+
+
+	}
+
